b7237bf63a4dfc907c5e88932d39747e87cb6abe
Ahmad878-code <affan.grewal73@gmail.com>
2025-12-07 15:47:36 +0500

    index

---
+++ index.py
@@ -0,0 +1,11 @@
+# Creating different variables
+x=5
+y="John"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+
+# user input
+name=input("Enter your name:")
+print("\nHello", name,"\nWelcome to codingal")
---
+++ main.py
@@ -0,0 +1,14 @@
+# print a sentence
+print("Welcome to the world of programming!")
+
+# print a number
+print(1)
+
+# Next line after print
+print("Hello Python \n")
+
+# Pass multiple arguments for printing
+print("Hello",5)
+
+# End argument of print statement
+print("Welcome to", end="*")
